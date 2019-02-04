750c615162d13bc5d26aaa47db65c02766735b3e
corrannolan <corrannolan3097@gmail.com>
2019-02-04 11:37:46 -0500

    the conflux
    dis boi  m o v e s

--- Repository/Assets/nofaller.cs
+++ Repository/Assets/nofaller.cs
@@ -6,6 +6,8 @@ public class nofaller : MonoBehaviour
 {
     Rigidbody rB;
 
+    float ran1, ran2, ran3;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +19,10 @@ public class nofaller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rB.angularVelocity = new Vector3(100, 50, 30);
+        ran1 = Random.Range(-100, 100);
+        ran2 = Random.Range(-100, 100);
+        ran3 = Random.Range(-100, 100);
+
+        rB.angularVelocity = new Vector3(ran1, ran2, ran3);
     }
 }
