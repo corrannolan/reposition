4dcf49ead71f72ac36ba7e20a73d75dc6c267d89
corrannolan <corrannolan3097@gmail.com>
2019-02-04 11:31:42 -0500

    dont fall
    it doesnt

---
+++ Repository/Assets/nofaller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nofaller : MonoBehaviour
+{
+    Rigidbody rB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rB = GetComponent<Rigidbody>();
+
+        rB.constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
