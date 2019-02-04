e17c2cc82c4564c5d9011babd0467b7e0499663e
corrannolan <corrannolan3097@gmail.com>
2019-02-04 11:41:31 -0500

    a connection
    on a spiritual level

---
+++ Repository/Assets/noodle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class noodle : MonoBehaviour
+{
+    Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = startPos;
+    }
+}
